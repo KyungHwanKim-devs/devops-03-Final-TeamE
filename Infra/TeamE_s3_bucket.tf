93a91fda009c6fcabbf647787e2f70b379062718
t99cm <too657@naver.com>
2023-03-07 14:51:29 +0900

    test.tf->infra/dev/TFremote 변경 및 IAM 추가

--- Infra/TeamE_s3_bucket.tf
+++ Infra/TeamE_s3_bucket.tf
@@ -12,9 +12,16 @@ resource "TeamE_s3_bucket_policy" "bucket_policy" {
       {
         Effect    = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::123456789012:root" # 다른 AWS 계정의 root 사용자에게 권한 부여
+          AWS = [
+          "arn:aws:iam::123456789012:root",
+          "arn:aws:iam::123456789012:user/example-user" # 다른 AWS 계정의 root 사용자에게 권한 부여
+          ]
         }
-        Action    = ["s3:GetObject"]
+        Action    = [
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:PutObject"
+        ]
         Resource  = "${TeamE_s3_bucket.bucket.arn}/*"
       }
     ]
