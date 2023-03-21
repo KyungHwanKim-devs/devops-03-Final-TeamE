984750670e43ef5cab18b60720494fffe05ea3a3
KyungHwanKim-devs <kkhdevs@gmail.com>
2023-03-22 05:58:18 +0900

    front api url 변경

--- front/apis/tweet.js
+++ front/apis/tweet.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export async function getAllTweets() {
-  const { data } = await axios(`http://localhost:8080/alltweets`);
+  const { data } = await axios(
+    `http://http://a680f12c80127465f883455a8b7edec1-2107361402.ap-northeast-2.elb.amazonaws.com/alltweets`,
+  );
 
   return data;
 }
